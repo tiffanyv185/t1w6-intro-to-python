f5daaf93dbe80b0c8e94f1610ac22aeb39a68859
tiffanyv185 <tiffanyv185@gmail.com>
2024-06-04 20:45:05 +1000

    variables

---
+++ another_case.py
@@ -0,0 +1,30 @@
+# variables
+# snake_case for python variables
+# camelCase for JavaSpcript
+# PascalCase for classes in all the languages
+# kebab-case
+
+num1 = 5
+num2 = 3
+
+print(num1)
+print(num1 + num2)
+
+num2 = 1
+
+num1 = num2 #assign the value of num2 to num 1
+
+print(num1 + num2)
+
+num3 = 5
+
+num3 = num3 + num2
+
+num3+= 3
+print(num3)
+
+total = num1 + num2 + num3
+
+# string interpolation
+print("The total addition is" , total)
+print (f"The total addition is {total}")
