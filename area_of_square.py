1ee780e65ca54301f1873b1792f426601e52026b
tiffanyv185 <tiffanyv185@gmail.com>
2024-06-04 21:01:57 +1000

    programs with inputs

---
+++ area_of_square.py
@@ -0,0 +1,8 @@
+# get the length from the user
+length = float(input("Enter the length of the square:"))
+# print(length)
+
+# calculate and store the area in a variable, area = length * length
+area = length * length
+# print the calculated area to the terminal
+print(area)
---
+++ create_email.py
@@ -0,0 +1,7 @@
+#Tiffany Vo Invenio - tiffany.vo@invenio.com
+first_name = input("Please enter your first name:")
+last_name = input("Please enter your last name:")
+company_name = input("Please enter your company name (no blank spaces)")
+
+print(f"{first_name}.{last_name}@{company_name}.com".lower())
+
